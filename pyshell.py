0dd502e31ce9c201fa34b4eb0a5b2f959a85b53c
enos <enyamor@gmail.com>
2025-11-23 06:48:51 -0800

    revisited the python scripting sys and subprocess libs

--- pyshell.py
+++ pyshell.py
@@ -4,4 +4,5 @@ import subprocess
 data = input("Enter a number : ")
 result = subprocess.run(['bash', './testi.sh', data], capture_output=True, text=True, check=True)
 
+
 print(result.stdout)
--- sys_lib.py
+++ sys_lib.py
@@ -2,16 +2,20 @@ import sys
 import subprocess
 import time
 
+# to understand len(sys.argv) better you must think of the '$#' in bash scripting
+# on the other had #@ and #* reflect the use of sys.argv, which is literally a container
+
+
 metrics = len(sys.argv)
 if metrics > 2:
-    print(x)
+    print("test")
     digit_one = sys.argv[1]
     digit_two = sys.argv[2]
 
-    print("digit two", digit_two)
-    print("digit one", digit_one)
+    #print("digit two", digit_two)
+    #print("digit one", digit_one)
     subprocess.run(['./sys.sh', digit_one, digit_two])
 else:
-    print("Thanks, error")
+    print("Error : Please provide two numbers as arguments")
     time.sleep(3)
 
