d9668af5fc9eeca2f1a86219a2911223a54b769b
enos <enyamor@gmail.com>
2026-01-07 12:40:47 -0800

    danger

--- zombie.sh
+++ zombie.sh
@@ -4,3 +4,10 @@ while true; do
     nohup yes > /dev/null 2>&1
 done
 
+# dont run this, BUDDY. A fork bomb will crash your system.
+# # A fork bomb is a type of denial-of-service attack against a computer system that makes use of the fork operation in Unix-like operating systems. The fork bomb works by creating a large number of processes very quickly, which can overwhelm the system's process table and lead to system instability or crashes.
+# # WARNING: DO NOT RUN THIS SCRIPT
+# # :(){ :|:& };:
+#
+#
+
